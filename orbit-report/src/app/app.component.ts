2ea4622e5f887a9c79d4c9b01a96bc936d47eeef
Vasavi <vasavi.gitla@yahoo.com>
2020-12-16 15:37:10 -0600

    Pass in Satellites to Orbit List Component

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -1,4 +1,4 @@
- import { Component } from '@angular/core';
+ import { Component, Input } from '@angular/core';
 import { Satellite } from './satellite';
 @Component({
   selector: 'app-root',
--- orbit-report/src/app/orbit-list/orbit-list.component.ts
+++ orbit-report/src/app/orbit-list/orbit-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-list',
   templateUrl: './orbit-list.component.html',
   styleUrls: ['./orbit-list.component.css']
 })
+// @Input() satellites: Satellite[];
 export class OrbitListComponent implements OnInit {
-
+  @Input() satellites: Satellite[];
   constructor() { }
 
   ngOnInit() {
